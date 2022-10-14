32a37c2e6c5bfdf9efe583822408b50f010e4cf7
Kayleigh Watts <kwatts949@gmail.com>
2022-10-14 09:58:00 +0100

    project setup

--- diary_project/lib/04_debugging_1/lib/most_common_letter.rb
+++ diary_project/lib/04_debugging_1/lib/most_common_letter.rb
@@ -1,12 +1,9 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
-  p counter
-  text.chars.each do |char|
-  p char
+  text.gsub(" ", "").chars.each do |char|
     counter[char] += 1
-  p counter
   end
-  p counter.to_a.sort_by { |k, v| v }[0][0]
+  p counter.to_a.sort_by { |k, v| v }[-1][0]
 end
 
 # Intended output:
